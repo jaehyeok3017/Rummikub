ae18fe08f0b7c7df8febd48f0839e0e12d0b4c60
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-10-19 14:14:23 +0900

    Initial commit

---
+++ src/model/Card.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Card {
+    public int cardNumber;
+    public String color;
+}
---
+++ src/model/Player.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Player {
+    public String name;
+    public int cardCount;
+    public int[] card = new int[54]; // 최대 카드 개수 106 & 최소 플레이 인원 2 == 54
+}
