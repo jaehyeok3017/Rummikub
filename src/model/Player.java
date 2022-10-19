24231136291ecb2b9e700dc67bdec665d9a34ea2
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-10-19 14:23:16 +0900

    Feat : `Player.java` Player constructor add
    - 플레이어 생성자 추가

--- src/model/Player.java
+++ src/model/Player.java
@@ -4,4 +4,8 @@ public class Player {
     public String name;
     public int cardCount;
     public int[] card = new int[54]; // 최대 카드 개수 106 & 최소 플레이 인원 2 == 54
+
+    public Player(String inputPlayerName){
+        name = inputPlayerName;
+    }
 }
