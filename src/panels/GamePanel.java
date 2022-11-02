058db29c8c3ef3f95146716275bead9b3c7d2fe2
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-02 11:16:41 +0900

    Feat : `GamePanel.java` KeyAdapter
    - 키보드 입력받기

--- src/panels/GamePanel.java
+++ src/panels/GamePanel.java
@@ -2,8 +2,8 @@ package panels;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
     public GamePanel(){
@@ -19,11 +19,18 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g){
-        
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+        }
     }
 }
