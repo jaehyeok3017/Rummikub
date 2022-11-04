8e1a2383f55efcca3a12088a776eee5edcbea1d4
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-04 10:05:03 +0900

    Feat : `GamePanel` GamePanel Show Complete
    - 900*500 크기의 판넬 띄우는 것까지 완료

--- src/panels/GamePanel.java
+++ src/panels/GamePanel.java
@@ -6,8 +6,21 @@ import java.awt.event.*;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
-    public GamePanel(){
 
+    static final int SCREEN_WIDTH = 900;
+    static final int SCREEN_HEIGHT = 500;
+
+    static final int LIMIT_TIME = 60; // turn-timer
+
+    Timer timer;
+    Random random;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new GameKeyAdapter());
+        startGame();
     }
 
     public void startGame(){
@@ -27,7 +40,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
-    public class MyKeyAdapter extends KeyAdapter{
+    public class GameKeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e) {
             super.keyPressed(e);
