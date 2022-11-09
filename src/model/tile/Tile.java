65f1bef6297c07879f903f02247473aabd61b6b4
lastro1206 <irene051206@naver.com>
2022-11-09 11:30:30 +0900

    feat `Tile.java` tileNum, tileColor지정

--- src/model/tile/Tile.java
+++ src/model/tile/Tile.java
@@ -3,13 +3,16 @@ package model.tile;
 public class Tile {
     final static int WIDTH = 120; //카드 가로
     final static int HEIGHT = 180; //카드 세로
+    public int tileNum;
+    public tileColor color;
+
+    public Tile(int tileNum, tileColor color){
+        this.tileNum = tileNum;
+        this.color = color;
+    }
 
     Boolean isJoker = false;
     Boolean isBatched = false;
-
-
-    //ArrayList<Integer> card = new ArrayList<Integer>();
-
     public Boolean jokerCheck(){
         return isJoker;
     }
