9cb82842567c7392f81e718e75f767359ca00726
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-11 18:00:15 +0900

    Feat : `Main.java` ArrayList

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,35 +2,44 @@ package game;
 
 import model.player.Player;
 import model.tile.tileColor;
-
 import java.util.Scanner;
 
 import static model.tile.color.*;
 
 public class Main {
     public static void main(String[] args) {
+        String[] tileList = new String[64];
 
         for(tileColor tile : tileColor.values()){
+            int i = 0;
+
             if(tile == tileColor.RED){
-                for(int i = 1; i < 14; i++){
-                    System.out.println(FONT_RED + "R" + i + RESET);
+                for(int j = 1; j < 14; j++){
+                    tileList[i++] = FONT_RED + "R" + j + RESET;
                 }
             }
+
             if(tile == tileColor.YELLOW){
-                for(int i = 1; i < 14; i++){
-                    System.out.println(FONT_YELLOW + "Y" + i + RESET);
+                for(int j = 1; j < 14; j++){
+                    tileList[i++] = FONT_YELLOW + "Y" + j + RESET;
                 }
             }
+
             if(tile == tileColor.BLUE){
-                for(int i = 1; i < 14; i++){
-                    System.out.println(FONT_BLUE + "B" + i + RESET);
+                for(int j = 1; j < 14; j++){
+                    tileList[i++] = FONT_BLUE + "B" + j + RESET;
                 }
             }
+
             if(tile == tileColor.WHITE){
-                for(int i = 1; i < 14; i++){
-                    System.out.println(FONT_WHITE + "W" + i + RESET);
+                for(int j = 1; j < 14; j++){
+                    tileList[i++] = FONT_WHITE + "W" + j + RESET;
                 }
             }
+        }
+
+        for(int i = 0; i<64; i++){
+            System.out.println(tileList[i]);
         }
 
         Scanner scan = new Scanner(System.in);
