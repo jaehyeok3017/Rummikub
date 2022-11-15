3fbc879c7e3d75f1bac1c9162ae29ef83d8032e6
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-16 01:00:58 +0900

    Feat : `Player.java` --> ArrayList 어캐할 지 생각해야함

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -1,16 +1,25 @@
 package model.player;
 
+import model.tile.Stack;
 import model.tile.Tile;
+
+import java.util.ArrayList;
 
 public class Player {
     public String name;
     public int cardCount;
-    public String[] card = new String[54]; // 최대 카드 개수 106 & 최소 플레이 인원 2 == 54
+    public static ArrayList<String> tileList = new ArrayList<String>(52); // 최대 카드 개수 106 & 최소 플레이 인원 2 == 54
 
-    public Player(String inputPlayerName){
+    public Player(String inputPlayerName) {
         name = inputPlayerName;
         cardCount = 0;
     }
 
+    public static void playerListShow(){
+        int length = tileList.size();
 
+        for(int i = 0; i < length; i++){
+            System.out.println(tileList.get(i));
+        }
+    }
 }
