1f8299991c2ea9c99fc3cca6e61b14e96e2e3b15
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-22 18:14:51 +0900

    Feat : `Player.java` Card Show Complete
    - 카드 섞고, 불러와서 보여주기까지 끝

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -8,18 +8,18 @@ import java.util.ArrayList;
 public class Player {
     public String name;
     public int cardCount;
-    public static ArrayList<String> tileList = new ArrayList<String>(52); // 최대 카드 개수 106 & 최소 플레이 인원 2 == 54
+    public ArrayList<String> tileList = new ArrayList<String>(52); // 최대 카드 개수 106 & 최소 플레이 인원 2 == 54
 
     public Player(String inputPlayerName) {
         name = inputPlayerName;
         cardCount = 0;
     }
 
-    public static void playerListShow(){
-        int length = tileList.size();
+    public static void playerListShow(ArrayList list){
+        int length = list.size();
 
         for(int i = 0; i < length; i++){
-            System.out.println(tileList.get(i));
+            System.out.print(list.get(i) + " ");
         }
     }
 }
