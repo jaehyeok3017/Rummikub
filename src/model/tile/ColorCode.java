c1d85d2b49da39e9f84d6eb078c8ff37335a933e
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-22 22:00:36 +0900

    Refactor : `ColorCode.java` Name Edit
    - 이름 조금 더 명확하도록 (컬러가 두개임)

--- src/model/tile/ColorCode.java
+++ src/model/tile/ColorCode.java
@@ -1,6 +1,6 @@
 package model.tile;
 
-public class Color {
+public class ColorCode {
     public static final String RESET = "\u001B[0m";
     public static String FONT_WHITE = "\u001B[37m"; //하얀색
     public static String FONT_RED = "\u001B[31m"; //빨강색
